fd404a507572912ce7f0cc9140cf6993739c3c3f
Tawl <tawl.tack@gmail.com>
2026-02-20 23:28:22 -0300

    chore: remove comments and improve estructure

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -8,16 +8,16 @@ type Props = {
 export default function StatCard({ title, value, extraInfo, subtitle }: Props) {
   return (
     <div className="bg-zinc-900 border border-zinc-800 rounded-lg p-6 shadow-xl hover:border-zinc-700 transition">
-        <>
-          <p className="text-sm text-zinc-400">{title}</p>
-          <div className="flex flex-col gap-2">
-            <h3 className="text-2xl font-semibold mt-2 tracking-tight">
-              {value}
-            </h3>
-            <a className="text-green-500 text-sm">{extraInfo}</a>
-          </div>
-          {subtitle && <p className="text-xs text-zinc-500 mt-2">{subtitle}</p>}
-        </>
+      <>
+        <p className="text-sm text-zinc-400">{title}</p>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-2xl font-semibold mt-2 tracking-tight">
+            {value}
+          </h3>
+          <a className="text-green-500 text-sm">{extraInfo}</a>
+        </div>
+        {subtitle && <p className="text-xs text-zinc-500 mt-2">{subtitle}</p>}
+      </>
     </div>
   );
 }
